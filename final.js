b7be41dcbe1d40dffc68ad562326276c5902a099
TahurAdnan <tahuradnan@gmail.com>
2023-12-13 11:27:39 +0600

    new addition

--- final.js
+++ final.js
@@ -5,14 +5,19 @@ function searchWeather() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             var cityElement = document.getElementById('city');
             var temperatureElement = document.getElementById('temperature');
             var descriptionElement = document.getElementById('description');
+            var windElement = document.getElementById('wind');
+            var visibility = document.getElementById('visibility');
             var iconElement = document.getElementById('weather-icon');
 
             cityElement.innerText = data.name;
             temperatureElement.innerText = `Temperature: ${Math.round(data.main.temp - 273.15)}°C`;
             descriptionElement.innerText = `Weather: ${data.weather[0].description}`;
+            windElement.innerText = `Wind: ${data.wind.speed}`;
+            visibility.innerText = `Visibility: ${data.visibility}`;
             iconElement.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
         })
         .catch(error => console.error('Error fetching weather:', error));
